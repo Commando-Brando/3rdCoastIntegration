f3b860808dc08f8989bbe2183da92e0f3aa94296
Commando-Brando <branmuller@gmail.com>
2021-12-25 15:49:30 -0600

    finished second row and made all elements responsive

---
+++ app.js
@@ -0,0 +1,11 @@
+// homepage JavaScript
+
+let titleImage = document.querySelector('#titleimage');
+let windowWidth = window.innerWidth;
+// if (width <= 1000) {
+//     titleImage.style.src = "/img/smalllogo.svg";
+// }
+// if (width <= 390) {
+//     titleImage.style.width = 300;
+//     titleImage.style.height = 300;
+// }
